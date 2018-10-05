33d50cae0ab7efbbbadd221f26e28bd6a3bfb092
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-10-05 17:19:01 -0300

    se agregan los import necesarios, y el sign in de google
    Falta terminar para que sea funcional

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/MainActivity.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/MainActivity.java
@@ -4,8 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+
+
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private TextView mStatusTextView;
+
+    private static final String TAG = "LoginActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     Button mBtnLogIn;
     Button mBtnSignIn;
@@ -14,6 +38,22 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                //PIDO EL TOKEN PARA ENVIAR AL SERVIDOR
+                //.requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build();
+
+        //Listeners
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         initButtons();
         createOnClickListenerButtons();
