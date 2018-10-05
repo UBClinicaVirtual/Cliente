9f7a74a355fb1b7c88c7e46984576f14f4b08f4a
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2018-10-05 03:40:46 -0300

    Navegacion entre activitys

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/MainActivity.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/MainActivity.java
@@ -1,13 +1,52 @@
 package pcs.ub.edu.ar.clinicavirtual;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnLogIn;
+    Button mBtnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initButtons();
+        createOnClickListenerButtons();
+    }
+
+    private void createOnClickListenerButtons() {
+
+        mBtnSignIn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent mIntentSignIn = new Intent(MainActivity.this, GoogleSignInActivity.class);
+                startActivity(mIntentSignIn);
+            }
+        } );
+
+        mBtnLogIn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent mIntentLogIn = new Intent(MainActivity.this, MainScreenActivity.class);
+                startActivity(mIntentLogIn);
+            }
+        } );
+
+
+    }
+
+
+    //the buttons are initialized
+    private void initButtons() {
+
+        mBtnLogIn = (Button) findViewById(R.id.btnLogIn);
+        mBtnSignIn =(Button) findViewById(R.id.btnSignIn);
+
     }
 }
---
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/MainScreenActivity.java
@@ -0,0 +1,86 @@
+package pcs.ub.edu.ar.clinicavirtual;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainScreenActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navMenu);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_screen, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_searchturn) {
+            //DIRIGE AL ACTIVITY SEARCH TURN
+            Intent mIntentSearchTurn = new Intent(MainScreenActivity.this, SearchTurnActivity.class);
+            startActivity(mIntentSearchTurn);
+        } else if (id == R.id.nav_myturns) {
+            //DIRIGE AL ACTIVITY MY TURNS
+            Intent mIntentMyTurns = new Intent(MainScreenActivity.this, MyTurnsActivity.class);
+            startActivity(mIntentMyTurns);
+
+        } else if (id == R.id.nav_myprofile) {
+            //DIRIGE AL ACTIVITY MY PROFILE
+            Intent mIntentMyProfile = new Intent(MainScreenActivity.this, ProfileActivity.class);
+            startActivity(mIntentMyProfile);
+
+        } else if (id == R.id.nav_logout) {
+            //DIRIGE AL ACTIVITY SIGN-IN
+            Intent mIntentLogOut = new Intent(MainScreenActivity.this, MainActivity.class);
+            startActivity(mIntentLogOut);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
