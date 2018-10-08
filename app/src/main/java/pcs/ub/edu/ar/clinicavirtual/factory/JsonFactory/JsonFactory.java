957d7df45c4363b7bdbdcf37b3b0e723e2037ddd
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2018-10-08 04:23:07 -0300

    Se crea un Json Factory

---
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/factory/JsonFactory/JsonFactory.java
@@ -0,0 +1,25 @@
+package pcs.ub.edu.ar.clinicavirtual.factory.JsonFactory;
+
+import org.json.JSONObject;
+
+import pcs.ub.edu.ar.clinicavirtual.data.UserData;
+
+public class JsonFactory {
+
+    public enum ObjectType{
+
+    }
+    public static JSONObject createJson( Object object){
+
+        JSONObject jsonObject = new JSONObject();
+
+        return null;
+    }
+
+    public static UserData convertJson(JSONObject jsonObject){
+
+        return  null;
+    }
+
+
+}
