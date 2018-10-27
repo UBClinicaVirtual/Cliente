95875e07af73ad55cbdb30462ba645377f38887f
Fernando Romero <fer.romero1991@gmail.com>
2018-10-26 22:01:00 -0300

    se agrega la clase PatientProfileDataException

---
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/exception/PatientProfileDataException/PatientProfileDataException.java
@@ -0,0 +1,10 @@
+package pcs.ub.edu.ar.clinicavirtual.data.exception.PatientProfileDataException;
+
+public class PatientProfileDataException extends RuntimeException {
+
+public PatientProfileDataException (String msg)
+{
+    super(msg);
+}
+
+}
