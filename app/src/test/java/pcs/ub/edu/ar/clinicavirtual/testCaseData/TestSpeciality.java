54f3d24089a25e9667a1a4fe831a3494cef422b0
Fernando Romero <fer.romero1991@gmail.com>
2018-10-30 13:45:31 -0300

    se agregan los test

--- app/src/test/java/pcs/ub/edu/ar/clinicavirtual/testCaseData/TestSpeciality.java
+++ app/src/test/java/pcs/ub/edu/ar/clinicavirtual/testCaseData/TestSpeciality.java
@@ -1,4 +1,30 @@
 package pcs.ub.edu.ar.clinicavirtual.testCaseData;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import pcs.ub.edu.ar.clinicavirtual.data.Speciality;
+import pcs.ub.edu.ar.clinicavirtual.data.exception.Speciality.SpecialityNullIdException;
+
 public class TestSpeciality {
+
+    @Test
+    public void testSpecialityNullId() {
+
+        try {
+            String mName = "Division gineco";
+            Integer mIdSpeciality = null;
+
+            Assert.assertNotNull(new Speciality(mIdSpeciality, mName));
+        } catch (SpecialityNullIdException e) {
+
+        }
+    }
+
+    @Test
+    public void testSpecialityIdIsZero() {
+        try{
+            String mName = ""
+        }
+    }
 }
