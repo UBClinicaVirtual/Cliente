4fab3f83338353dd3dbb451afdb15ff46dd1e6d0
Fernando Romero <fer.romero1991@gmail.com>
2018-11-02 13:37:24 -0300

    se crea la exception userdata

---
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/exception/UserData/UserDataException.java
@@ -0,0 +1,7 @@
+package pcs.ub.edu.ar.clinicavirtual.data.exception.UserData;
+
+public class UserDataException extends RuntimeException {
+    public UserDataException (String msg){
+        super (msg);
+    }
+}
