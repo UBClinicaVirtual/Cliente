06026fda5aee3eed6eae6c2f464dda03db589d31
Fernando Romero <fer.romero1991@gmail.com>
2018-11-02 13:35:39 -0300

    se agregan las validaciones con sus excepciones correspondientes

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/UserData.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/UserData.java
@@ -1,5 +1,13 @@
 package pcs.ub.edu.ar.clinicavirtual.data;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import pcs.ub.edu.ar.clinicavirtual.data.exception.UserData.UserDataIdIsZeroException;
+import pcs.ub.edu.ar.clinicavirtual.data.exception.UserData.UserDataInvalidEmailException;
+import pcs.ub.edu.ar.clinicavirtual.data.exception.UserData.UserDataNullEmailException;
+import pcs.ub.edu.ar.clinicavirtual.data.exception.UserData.UserDataNullIdException;
+import pcs.ub.edu.ar.clinicavirtual.data.exception.UserData.UserDataNullNameException;
 import pcs.ub.edu.ar.clinicavirtual.interfaces.IUserProfileData;
 
 public class UserData implements IUserProfileData {
@@ -19,6 +27,10 @@ public class UserData implements IUserProfileData {
     }
 
     private void setmID(Integer mID) {
+        if (mID == null)
+            throw new UserDataNullIdException();
+        if (mID == 0)
+            throw new UserDataIdIsZeroException();
         this.mID = mID;
     }
     @Override
@@ -27,6 +39,8 @@ public class UserData implements IUserProfileData {
     }
 
     private void setmName(String mName) {
+        if ((mName == null) || (mName.trim().isEmpty()))
+            throw new UserDataNullNameException();
         this.mName = mName;
     }
     @Override
@@ -35,7 +49,27 @@ public class UserData implements IUserProfileData {
     }
 
     private void setmEmail(String mEmail) {
+        if((mEmail == null) || (mEmail.trim().isEmpty()))
+            throw new UserDataNullEmailException();
+        if(!validarmail(mEmail))
+            throw new UserDataInvalidEmailException();
+
         this.mEmail = mEmail;
+    }
+
+    private boolean validarmail(String mEmail) {
+
+        //Pattern to validate the email
+        Pattern pattern = Pattern
+                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher mather = pattern.matcher(mEmail);
+
+        if (mather.find() == true){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
