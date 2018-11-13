468ed5eb1569d760aa03379b5a4735d25e21a44d
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-11-13 17:48:09 -0300

    se crea el holder para turnos

---
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/AppointmentHolder.java
@@ -0,0 +1,11 @@
+package pcs.ub.edu.ar.clinicavirtual.data;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+class AppointmentHolder extends RecyclerView.ViewHolder {
+    public AppointmentHolder(View view, AppointmentItemListener mItemListener) {
+        super();
+
+    }
+}
