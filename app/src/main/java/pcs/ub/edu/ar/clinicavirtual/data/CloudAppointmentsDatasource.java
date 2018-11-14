0f195829bece5779e40bf0d7775dd7d11c270268
Fernando Romero <fer.romero1991@gmail.com>
2018-11-14 13:08:19 -0300

    se crea la clase cloduappointmentsdatasource, por ahora simula la conexion, pendiente cargar el json obtenido del servidor

---
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/CloudAppointmentsDatasource.java
@@ -0,0 +1,40 @@
+package pcs.ub.edu.ar.clinicavirtual.data;
+
+import android.os.Handler;
+
+import com.google.api.client.util.Lists;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+
+public class CloudAppointmentsDatasource implements ICloudAppointmentsDataSource {
+
+    private static HashMap<String, Appointment> API_DATA;
+    private static final long LATENCY = 2000;
+
+    static {
+        API_DATA = new LinkedHashMap<>();
+        for (int i = 0; i<15; i++){
+            addAppointment("Hospital de clinicas" , "14/11/2018 12:27 pm", "Dr. Duartes Walter", "Pendiente",
+                    "file:///android_asset/logo.jpg");
+        }
+    }
+
+    private static void addAppointment(String clinic, String date, String doctor, String status,
+                                       String imageUrl){
+        Appointment newAppointment = new Appointment(clinic, date, doctor,status,imageUrl);
+        API_DATA.put(newAppointment.getID(),newAppointment);
+    }
+
+    @Override
+    public void getAppointments(final AppointmentServiceCallback callback) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                callback.onLoaded(Lists.newArrayList(API_DATA.values()));
+            }
+            }, LATENCY);
+    }
+}
