5b6608ab57938d83767bb70326ffecebcc0a891d
Fernando Romero <fer.romero1991@gmail.com>
2018-11-14 13:30:33 -0300

    se crea el dependencyuprovide para qutiarle responsabilidad al prseentador de crear el reposotorio segun corresponda

---
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/DependencyProvider.java
@@ -0,0 +1,39 @@
+package pcs.ub.edu.ar.clinicavirtual.data;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public final class DependencyProvider {
+
+    private static Context mContext;
+    private static MemoryAppointmentsDataSource memorySource = null;
+    private static CloudAppointmentsDatasource cloudSource = null;
+    private static AppointmentRepository mAppointmentsRepository = null;
+
+    private DependencyProvider() {
+    }
+
+    public static AppointmentRepository provideAppointmentsRepository(@NonNull Context context) {
+        mContext = checkNotNull(context);
+        if (mAppointmentsRepository == null) {
+            mAppointmentsRepository = new AppointmentRepository(getMemorySource(),
+                    getCloudSource(), context);
+        }
+        return mAppointmentsRepository;
+    }
+
+    public static MemoryAppointmentsDataSource getMemorySource() {
+        if (memorySource == null) {
+            memorySource = new MemoryAppointmentsDataSource();
+        }
+        return memorySource;
+    }
+
+    public static CloudAppointmentsDatasource getCloudSource() {
+        if (cloudSource == null) {
+            cloudSource = new CloudAppointmentsDatasource();
+        }
+        return cloudSource;
+    }
+}
