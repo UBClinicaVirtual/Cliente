a5f49f8308d89760969b4c4cc797a1c3ec21783f
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-11-14 17:56:02 -0300

    Fixs

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/activitys/general/MyTurnsActivity.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/activitys/general/MyTurnsActivity.java
@@ -5,6 +5,8 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,8 +18,8 @@ import pcs.ub.edu.ar.clinicavirtual.handler.GetPatientAppointmentsHandler;
 
 public class MyTurnsActivity extends BaseActivity {
 
-    Button mSearchMyTurns;
-    TextView mMyTurns;
+    //Button mSearchMyTurns;
+    //TextView mMyTurns;
 
     private Toolbar mToolbar;
     private Fragment mTurnsFragment;
@@ -26,11 +28,16 @@ public class MyTurnsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_turns);
-        initScreen();
-        initElements();
-        initListeners();
-    }
+        //initScreen();
+       // initElements();
+       // initListeners();
+       // mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mTurnsFragment = getSupportFragmentManager().findFragmentById(R.id.turns_container);
 
+       // setUpToolbar();
+        setUpTurnsFragment();
+    }
+/*
     private void initScreen() {
         android.support.v7.app.ActionBar bar = getSupportActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(Color.RED));
@@ -41,8 +48,8 @@ public class MyTurnsActivity extends BaseActivity {
     }
 
     private void initElements() {
-        mSearchMyTurns = (Button) findViewById(R.id.btnPatientMyTurns);
-        mMyTurns = (TextView) findViewById(R.id.txtMyTurns);
+       // mSearchMyTurns = (Button) findViewById(R.id.btnPatientMyTurns);
+       // mMyTurns = (TextView) findViewById(R.id.txtMyTurns);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mTurnsFragment = getSupportFragmentManager().findFragmentById(R.id.turns_container);
         
@@ -51,7 +58,7 @@ public class MyTurnsActivity extends BaseActivity {
 
 
     }
-
+*/
     private void setUpTurnsFragment() {
         if(mTurnsFragment == null){
             mTurnsFragment = TurnsFragment.newInstance();
@@ -61,34 +68,59 @@ public class MyTurnsActivity extends BaseActivity {
                     .commit();
         }
     }
-
+/*
     private void setUpToolbar() {
         setSupportActionBar(mToolbar);
     }
+*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_turns, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+/*
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+*/
+        return super.onOptionsItemSelected(item);
+    }
 
     /**
      * Called when a view has been clicked.
      *
      * @param v The view that was clicked.
      */
+
     @Override
     public void onClick(View v) {
+/*
         switch (v.getId()) {
             case R.id.btnPatientMyTurns:
                 ServerRequestUserGetPatientAppointments getPatientAppointments = new ServerRequestUserGetPatientAppointments(R.id.btnPatientMyTurns);
                 getPatientAppointments.apiToken( apitoken() );
                 connector().execute(getPatientAppointments,this);
+
         }
+*/
     }
 
     @Override
     protected void loadNextActivityHandler() {
 
     }
-
+/*
     @Override
     protected void loadHandlers() {
         this.handlers().put(R.id.btnPatientMyTurns, new GetPatientAppointmentsHandler());
     }
+    */
 }
