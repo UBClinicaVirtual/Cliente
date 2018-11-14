b6ee6e4fa89e6f0dd4e1d09c140a05542a211ef2
Fernando Romero <fer.romero1991@gmail.com>
2018-11-14 13:07:55 -0300

    se cre la interfaz iappointmentsrepository

---
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/IAppointmentsRepository.java
@@ -0,0 +1,12 @@
+package pcs.ub.edu.ar.clinicavirtual.data;
+
+import java.util.List;
+
+public interface IAppointmentsRepository {
+    interface GetAppointmentsCallback{
+        void onAppointmentsLoaded(List<Appointment> appointments);
+        void onDataNotAvailable (String error);
+    }
+    void getAppointments(GetAppointmentsCallback callback);
+    void refreshAppointments();
+}
