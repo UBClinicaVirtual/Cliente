095171b5ae014b868835ff8153e10ec349c7d43d
Fernando Romero <fer.romero1991@gmail.com>
2018-11-14 10:52:42 -0300

    se agrega los metodos setupappointlmentlist, setuprefreshlayout al fragment

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/activitys/general/TurnsFragment.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/activitys/general/TurnsFragment.java
@@ -3,6 +3,7 @@ package pcs.ub.edu.ar.clinicavirtual.activitys.general;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -57,7 +58,37 @@ public class TurnsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_turns, container , false);
 
         //referencias ui
-        mAppointmentList = (RecyclerView) root.findViewById(R.id.appointment_)
+        mAppointmentList = (RecyclerView) root.findViewById(R.id.appointment_list);
+        mEmptyView = root.findViewById(R.id.noAppointment);
+        mSwipeRefreshLayout = (SwipeRefreshLayout)
+                root.findViewById(R.id.refresh_layout);
+
+        //setup
+        setUpAppointmentList();
+        setUpRefreshLayout();
+
+        return root;
+
+    }
+
+    private void setUpAppointmentList() {
+        mAppointmentList.setAdapter(mAppointmentAdapter);
+        mAppointmentList.setHasFixedSize(true);
+    }
+
+    //cambiar el esquema de colores del indicador
+    private void setUpRefreshLayout(){
+        mSwipeRefreshLayout.setColorSchemeColors(
+                ContextCompat.getColor(getActivity(),R.color.colorPrimary),
+                ContextCompat.getColor(getActivity(), R.color.colorAccent),
+                ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+            }
+        });
 
     }
 }
