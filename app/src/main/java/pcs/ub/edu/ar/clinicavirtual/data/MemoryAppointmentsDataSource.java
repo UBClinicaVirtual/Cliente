fd9790d396f2653c8061d7fd6cd8d476d3e4c54a
Fernando Romero <fer.romero1991@gmail.com>
2018-11-14 13:07:04 -0300

    se agrega clase memoryappointmentsdatasource, para utilizar los turnos en memoria

---
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/MemoryAppointmentsDataSource.java
@@ -0,0 +1,41 @@
+package pcs.ub.edu.ar.clinicavirtual.data;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class MemoryAppointmentsDataSource implements IMemoryAppointmentsDataSource {
+
+    private static HashMap<String, Appointment> mCachedAppointments;
+
+    @Override
+    public List<Appointment> find(AppointmentCriteria criteria) {
+        ArrayList<Appointment> appointments = Lists.newArrayList(mCachedAppointments.values());
+        return appointments;
+    }
+
+    @Override
+    public void save(Appointment appointment) {
+        if(mCachedAppointments == null){
+            mCachedAppointments = new LinkedHashMap<>();
+        }
+        mCachedAppointments.put(appointment.getID(), appointment);
+
+    }
+
+    @Override
+    public void deleteAll() {
+        if(mCachedAppointments == null){
+            mCachedAppointments = new LinkedHashMap<>();
+        }
+        mCachedAppointments.clear();
+    }
+
+    @Override
+    public boolean mapIsNull() {
+        return mCachedAppointments == null;
+    }
+}
