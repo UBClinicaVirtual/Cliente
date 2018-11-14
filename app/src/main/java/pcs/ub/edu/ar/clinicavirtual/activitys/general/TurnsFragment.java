0c58b624b852c7a78c53fafceadd5baf46cda86b
Fernando Romero <fer.romero1991@gmail.com>
2018-11-14 10:28:33 -0300

    se le agrega un reclyclerview, adapta,r, swipe repfresh

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/activitys/general/TurnsFragment.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/activitys/general/TurnsFragment.java
@@ -3,11 +3,17 @@ package pcs.ub.edu.ar.clinicavirtual.activitys.general;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import pcs.ub.edu.ar.clinicavirtual.R;
+import pcs.ub.edu.ar.clinicavirtual.data.Appointment;
+import pcs.ub.edu.ar.clinicavirtual.data.AppointmentAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,52 +21,43 @@ import pcs.ub.edu.ar.clinicavirtual.R;
  * create an instance of this fragment.
  */
 public class TurnsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView mAppointmentList;
+    private AppointmentAdapter mAppointmentAdapter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private View mEmptyView;
+    private AppointmentAdapter.AppointmentItemListener mItemListener = new AppointmentAdapter.AppointmentItemListener() {
+        @Override
+        public void onAppointmentClick(Appointment clickedNote) {
+            // Pendiente armar la pantalla detalle de los turnos
+        }
+    };
 
 
     public TurnsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TurnsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TurnsFragment newInstance(String param1, String param2) {
-        TurnsFragment fragment = new TurnsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static TurnsFragment newInstance() {
+        return new TurnsFragment();
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mAppointmentAdapter = new AppointmentAdapter(new ArrayList<Appointment>(0), mItemListener);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_turns, container, false);
-    }
+        //conseguir todos los elemtnos de la inte
+        View root = inflater.inflate(R.layout.fragment_turns, container , false);
 
+        //referencias ui
+        mAppointmentList = (RecyclerView) root.findViewById(R.id.appointment_)
+
+    }
 }
