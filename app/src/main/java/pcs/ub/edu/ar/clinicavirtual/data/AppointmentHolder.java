96c49dbfe6026448a05ac26ae34bdbe2b74df3ca
Fernando Romero <fer.romero1991@gmail.com>
2018-11-14 09:54:08 -0300

    hago el findviewbyid de los items en item_appointment para el appointmentholder

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/AppointmentHolder.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/AppointmentHolder.java
@@ -2,26 +2,35 @@ package pcs.ub.edu.ar.clinicavirtual.data;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import pcs.ub.edu.ar.clinicavirtual.R;
+/*
 class AppointmentHolder  extends RecyclerView.ViewHolder implements View.OnClickListener{
 
     public TextView mclinic_name;
     public TextView mdate;
     public TextView mhcp;
     public TextView mstatusAppointment;
+    public ImageView featuredImage;
 
     private AppointmentItemListener mAppointmentItemListener;
 
     public AppointmentHolder(View itemView, AppointmentItemListener listener) {
         super(itemView);
         mAppointmentItemListener = listener;
-        mclinic_name = (TextView) itemView.findViewById(R.id.)
-
+        mclinic_name = (TextView) itemView.findViewById(R.id.clinic_name);
+        mdate = (TextView) itemView.findViewById(R.id.appointment_date);
+        mhcp = (TextView) itemView.findViewById(R.id.hcp_name);
+        mstatusAppointment = itemView.findViewById(R.id.appointment_status);
+        featuredImage = (ImageView) itemView.findViewById(R.id.appointment_featured_image);
     }
 
     @Override
     public void onClick(View view) {
-
+        int position = getAdapterPosition();
+       // Appointment appointment = getitem(position);
     }
 }
+*/
