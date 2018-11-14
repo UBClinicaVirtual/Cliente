8ca12198d3fd849fc447403f92ec406cbffb4048
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-11-13 22:07:37 -0300

    se agerga constructor al holder

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/AppointmentHolder.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/data/AppointmentHolder.java
@@ -2,10 +2,26 @@ package pcs.ub.edu.ar.clinicavirtual.data;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
-class AppointmentHolder extends RecyclerView.ViewHolder {
-    public AppointmentHolder(View view, AppointmentItemListener mItemListener) {
-        super();
+class AppointmentHolder  extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView mclinic_name;
+    public TextView mdate;
+    public TextView mhcp;
+    public TextView mstatusAppointment;
+
+    private AppointmentItemListener mAppointmentItemListener;
+
+    public AppointmentHolder(View itemView, AppointmentItemListener listener) {
+        super(itemView);
+        mAppointmentItemListener = listener;
+        mclinic_name = (TextView) itemView.findViewById(R.id.)
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
