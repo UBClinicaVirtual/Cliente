6d5e4a3e28affedb578ad696acdd145c6c0600f1
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-11-14 15:51:31 -0300

    se agerga las condiciones de carga en el infinitescroll

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/InfiniteScrollListener.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/InfiniteScrollListener.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.RecyclerView.OnScrollListener;
 
 import pcs.ub.edu.ar.clinicavirtual.data.DataLoading;
 
+import static com.google.android.gms.internal.firebase_auth.zzv.checkNotNull;
+
 public abstract class InfiniteScrollListener extends OnScrollListener {
 
     //cantidad requerida de items visibles antes del final, para cargar una nueva pag
@@ -17,12 +19,28 @@ public abstract class InfiniteScrollListener extends OnScrollListener {
 
 
     public InfiniteScrollListener(DataLoading dataLoading, LinearLayoutManager linearLayoutManager){
-        
+        mDataLoading = checkNotNull(dataLoading);
+        mLayoutManager = checkNotNull(linearLayoutManager);
     }
+
+
+    // si la cnatidad total de i. menos la cant de visibles, es menor o igual que la suma
+    // del primer item mas el threshold, entonces se inicia otra carga
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        if (dy < 0 || mDataLoading.isLoadingData() || !mDataLoading.isThereMoreData())
+            return;
 
+        final int visibleItemCount = recyclerView.getChildCount();
+        final int totalItemCount = mLayoutManager.getItemCount();
+        final int firstVisibleItem = mLayoutManager.findFirstVisibleItemPosition();
+
+        if ((totalItemCount - visibleItemCount) <= (firstVisibleItem + VISIBLE_THRESHOLD)){
+            onLoadMore();
+        }
     }
+
+    public abstract void onLoadMore();
 
 
 }
