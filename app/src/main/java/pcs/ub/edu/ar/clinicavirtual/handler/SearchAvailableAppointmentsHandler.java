6e772842ee01efb9ac544c5915362dcc11427517
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-11-16 13:01:20 -0300

    fix

--- app/src/main/java/pcs/ub/edu/ar/clinicavirtual/handler/SearchAvailableAppointmentsHandler.java
+++ app/src/main/java/pcs/ub/edu/ar/clinicavirtual/handler/SearchAvailableAppointmentsHandler.java
@@ -2,7 +2,10 @@ package pcs.ub.edu.ar.clinicavirtual.handler;
 
 import android.widget.Toast;
 
+import org.json.JSONException;
+
 import pcs.ub.edu.ar.clinicavirtual.activitys.base.BaseActivity;
+import pcs.ub.edu.ar.clinicavirtual.activitys.general.MyTurnsActivity;
 import pcs.ub.edu.ar.clinicavirtual.activitys.general.SearchTurnActivity;
 import pcs.ub.edu.ar.clinicavirtual.connection.facade.pattern.connection.requests.appointment.ServerRequestSearchAvailableAppointments;
 import pcs.ub.edu.ar.clinicavirtual.interfaces.facade.pattern.connection.IServerRequest;
@@ -13,8 +16,16 @@ public class SearchAvailableAppointmentsHandler implements IServerResponseHandle
     public void handle(IServerRequest request, BaseActivity activity) {
 
         ServerRequestSearchAvailableAppointments requestSeachAppointments = (ServerRequestSearchAvailableAppointments) request;
+        String response = requestSeachAppointments.response();
         SearchTurnActivity searchTurnActivity = (SearchTurnActivity) activity;
 
-        Toast.makeText(searchTurnActivity, requestSeachAppointments.response() , Toast.LENGTH_SHORT).show();
+        try {
+            searchTurnActivity.loadAppointment(response);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //Toast.makeText(searchTurnActivity, requestSeachAppointments.response() , Toast.LENGTH_SHORT).show();
+
     }
 }
